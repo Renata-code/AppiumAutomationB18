2cf6db979ef7a27fb159546403d4da6c0c213e3b
Vasyl Fomiuk <vasyl@cybertekschool.com>
2020-09-14 16:51:02 -0400

    added 1st tests

--- src/test/java/com/cybertek/tests/CalculatorTests.java
+++ src/test/java/com/cybertek/tests/CalculatorTests.java
@@ -3,17 +3,11 @@ package com.cybertek.tests;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
-import io.appium.java_client.remote.AndroidMobileCapabilityType;
-import io.appium.java_client.remote.MobileCapabilityType;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
-
-import java.net.MalformedURLException;
 import java.net.URL;
 
 public class CalculatorTests {
@@ -53,6 +47,7 @@ public class CalculatorTests {
     @Test
     public void test1() {
         System.out.println("Test 1");
+
         AndroidElement btn2 = driver.findElementById("com.android.calculator2:id/digit_2");
         AndroidElement equals = driver.findElementByAccessibilityId("equals");
         AndroidElement plus = driver.findElementByAccessibilityId("plus");
